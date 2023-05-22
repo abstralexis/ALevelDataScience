e3cdfcf1690d8b09a10836d074faeacad9824b71
AlexisComix <thecomixguy@hotmail.com>
2023-05-22 10:17:04 +0100

    Add some CREATE TABLE queries and fix ERD

--- parse_data.py
+++ parse_data.py
@@ -10,17 +10,62 @@ COLUMN_NAMES_INDEX = 5
 # Data starts
 # --------------------------------------------------------------
 
-with open(
-          "data\Large Data Set - Beijing May-Oct 1987.csv",
-          "r") as file:
-    data = csv.reader(file)
-    for index, row in enumerate(data):
-        if index in set([NAME_INDEX,
-                        NGR_INDEX,
-                        ALTITUDE_INDEX,
-                        LATITUDE_INDEX]):
-            print(row[0])
-        elif index == COLUMN_NAMES_INDEX:
-            print(row)
-            
-     
+"""--sql
+CREATE TABLE IF NOT EXISTS LocationNames (
+    UniqueID INT PRIMARY KEY AUTOINCREMENT,
+    LocationName VARCHAR NOT NULL,
+);
+"""
+
+"""--sql
+CREATE TABLE IF NOT EXISTS BeaufortUnits (
+    UniqueID INT PRIMARY KEY AUTOINCREMENT,
+    UnitName VARCHAR NOT NULL,
+);
+"""
+
+"""--sql
+CREATE TABLE IF NOT EXISTS CardinalDirections (
+    UniqueID INT PRIMARY KEY AUTOINCREMENT,
+    Direction VARCHAR NOT NULL,    
+);
+"""
+
+"""--sql
+CREATE TABLE IF NOT EXISTS LocalData (
+    LocationID INT FOREIGN KEY REFERENCES LocationNames(UniqueID),
+    DataYear INT NOT NULL,
+    DataMonth INT NOT NULL,
+    DataDay INT NOT NULL,
+    MeanAirTempC REAL,
+    TotalRainfallMm REAL,
+    TotalSunshineHrs REAL,
+    MeanWindspeedKn INT,
+    MeanWindspeedBft INT FOREIGN KEY REFERENCES BeaufortUnits(UniqueID),
+    MaxGustKn INT,
+    MaxHumidityPercent INT,
+    MeanCloudOktas INT,
+    MeanVisibilityDm INT,
+    MeanPressureHpa INT,
+    MeanWindDirDeg INT,
+    MeanWindDirCardinal INT FOREIGN KEY REFERENCES CardinalDirections(UniqueID),
+    MaxGustDirDeg INT,
+    MaxGustDirCardinal INT FOREIGN KEY REFERENCES CardinalDirections(UniqueID),
+    PRIMARY KEY (LocationID, DataYear, DataMonth, DataDay),
+);
+"""
+
+"""--sql
+CREATE TABLE IF NOT EXISTS OverseasData (
+    LocationID INT FOREIGN KEY REFERENCES LocationNames(UniqueID),
+    DataYear INT NOT NULL,
+    DataMonth INT NOT NULL,
+    DataDay INT NOT NULL,
+    MeanAirTempC REAL,
+    TotalRainfallMm REAL,
+    MeanPressureHpa INT,
+    MeanWindspeedKn INT,
+    MeanWindspeedBft INT FOREIGN KEY REFERENCES BeaufortUnits(UniqueID),
+    PRIMARY KEY (LocationID, DataYear, DataMonth, DataDay),
+);
+"""  
